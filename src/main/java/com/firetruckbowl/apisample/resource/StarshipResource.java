99648caf9172199d8b5c5d3831278506d71e05c9
Loc Nguyen <lochnguyen@gmail.com>
2011-09-25 02:28:04 -0700

    Implementing Documented interface so hitting the resource URL with OPTIONS returns the document

--- src/main/java/com/firetruckbowl/apisample/resource/StarshipResource.java
+++ src/main/java/com/firetruckbowl/apisample/resource/StarshipResource.java
@@ -1,16 +1,22 @@
 package com.firetruckbowl.apisample.resource;
 
 import com.firetruckbowl.apisample.model.Starship;
+import com.firetruckbowl.tgirest.annotation.ParamNote;
+import com.firetruckbowl.tgirest.annotation.ResourceDoc;
+import com.firetruckbowl.tgirest.annotation.ResourceMethod;
+import com.firetruckbowl.tgirest.model.ResourceDocument;
+import com.firetruckbowl.tgirest.processor.TGIRestDocumenter;
+import com.firetruckbowl.tgirest.resource.Documented;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
 
+@ResourceDoc(description = "Provides a means to manage the starships in your fleet")
 @Path("starship")
-public class StarshipResource {
+public class StarshipResource implements Documented {
 
   @GET
   @Path("hello")
@@ -22,12 +28,20 @@ public class StarshipResource {
   @GET
   @Path("{id}")
   @Produces(MediaType.APPLICATION_JSON)
-  public Response getStarship(@PathParam("id") String id) {
+  @ResourceMethod(description = "Get a starship by its ID", status = Response.Status.OK)
+  public Response getStarship(@PathParam("id") @ParamNote("The starship ID") String id) {
     Starship pegasus = new Starship();
     pegasus.setId(id);
     pegasus.setName("Pegasus");
     pegasus.setModel("Mercury");
 
     return Response.status(Response.Status.OK).entity(pegasus).build();
+  }
+
+  @Override
+  public Response getDocumentation(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders) {
+    TGIRestDocumenter documenter = new TGIRestDocumenter();
+    ResourceDocument doc = documenter.generateResourceDocument(uriInfo, this);
+    return Response.status(Response.Status.OK).entity(doc).build();
   }
 }
